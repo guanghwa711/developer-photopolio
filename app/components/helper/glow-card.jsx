52cf43ab6c050b63a1b3dfdda5b02426d112a167
guanghwa711 <guanghwa711@gmail.com>
2025-01-27 03:45:42 +0200

    resolve issue

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 const GlowCard = ({ children, identifier }) => {
   useEffect(() => {
-    if (typeof window === "undefined") return; // Ensure this runs only in the browser
+    if (typeof document === "undefined") return; // Ensure this runs only in the browser
 
     const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
     const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
